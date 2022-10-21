730cc8f7e4aba57ada982d99fe3b9b8c665c25d9
DimitriiTrater <dima1603040@gmail.com>
2022-10-21 08:17:00 +0200

    Start 3task 4lab

--- 4lab/Source Files/3task.cpp
+++ 4lab/Source Files/3task.cpp
@@ -2,9 +2,22 @@
 // Created by 1 on 20.10.2022
 //
 #include <iostream>
+#include <ctime>
+
 
 using namespace std;
 
+void changeArray(int array[], int SIZE) {
+
+}
+
 int generalFunctionOfTaskThreeLabFour() {
+    srand(time(0));
+    int SIZE = 15;
+    int array[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = rand() % 7;
+    }
+    changeArray(array, SIZE);
     return 0;
 }
