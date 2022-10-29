d9de0f403cd801dba7c673dbb667da6ea1fa110e
DimitriiTrater <dima1603040@gmail.com>
2022-10-29 19:10:36 +0200

    Done 4task

--- 4lab/Source Files/4task.cpp
+++ 4lab/Source Files/4task.cpp
@@ -11,27 +11,37 @@ using namespace std;
 void bubleSort(int array[], int size) {
     while (size--) {
         bool swapped = false;
-        for(int i = 0; i < size; i++)
-		{
-			if(array[i] > array[i + 1])
-			{
+        for(int i = 0; i < size; i++) {
+			if(array[i] > array[i + 1]) {
 				swap(array[i], array[i + 1]);
 				swapped = true;
 			}
 		}
 		
-		if(swapped == false)
+        if(swapped == false){
 			break;
+        }
     }
 
 }
 
-int printArray(int array[], int size) {
+int counter(int number, int array[], int size) {
+    int num_count = 0;
     for (int i = 0; i < size; i++) {
-        cout << array[i];
-        while (array[i]) {
-            cout << "*";
-            array[i]--;
+        if (number == array[i]) {
+            num_count++;
+        }
+    }
+    return num_count;
+}
+
+
+int printArray(int array[], int size) {
+    for (int i = 0; i <= 20; i++) {
+        cout << i << " ";
+        int b = counter(i, array, size);
+        for (int a = 0; a < b; a++) {
+            cout << "-";
         }cout << endl;
     }
     return 0;
@@ -41,18 +51,25 @@ int printArray(int array[], int size) {
 int generalFunctionOfTaskFourLabFour() {
     unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
     default_random_engine eng(seed);
+
     cout << "Enter the size array" << endl;
     int size = 0;
     cin >> size;
+    
     int* array = new int[size];
     for (int i = 0; i < size; i++) {
         array[i] = eng() % 20;
     }
+    
     bubleSort(array, size);
+    
     for (int i = 0; i < size; i++) {
-        cout << array[i] << "\t";
+        cout << array[i] << " ";
     } cout << endl;
+    
     printArray(array, size);
+    
     delete[] array;
+    
     return 0;
 }
---
+++ 4lab/Source Files/7task.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int generalFunctionOfTaskSevenLabFour() {
+    int a[2][2] = {
+        {1, 1},
+        {1, 1},
+    };
+
+    int b[2][2] = {
+        {2, 2},
+        {2, 2},
+    };
+
+    int c[2][2] = {
+        {0, 0},
+        {0, 0},
+    };
+    
+    return 0;
+}
