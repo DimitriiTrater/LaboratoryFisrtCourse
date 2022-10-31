b19a7142851780cb487577b2ebe6aac6789d56c0
DimitriiTrater <dima1603040@gmail.com>
2022-10-31 13:23:39 +0200

    Add readme 3 lab chng cmakelist complete 7 task

--- 4lab/Source Files/7task.cpp
+++ 4lab/Source Files/7task.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 
+using namespace std;
+
+
+void matrixSum(const int rows, const int cols, int A[][2], int B[][2], int C[][2]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << C[i][j] << "\t";
+        } cout << endl;
+    }
+}
+
+
+
+
 
 int generalFunctionOfTaskSevenLabFour() {
-    int a[2][2] = {
+    const int rows = 2;
+    const int cols = 2;
+
+    int a[rows][cols] = {
         {1, 1},
         {1, 1},
     };
 
-    int b[2][2] = {
+    int b[rows][cols] = {
         {2, 2},
         {2, 2},
     };
 
-    int c[2][2] = {
+    int c[rows][cols] = {
         {0, 0},
         {0, 0},
     };
     
+    matrixSum(rows, cols, a, b, c);
     return 0;
 }
