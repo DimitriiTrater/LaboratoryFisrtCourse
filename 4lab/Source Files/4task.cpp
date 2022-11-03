86e7a8fbce8a9881f6bdb75cbcee126b04371101
DimitriiTrater <dima1603040@gmail.com>
2022-11-03 23:33:14 +0200

    Fix some bug in 4 task

--- 4lab/Source Files/4task.cpp
+++ 4lab/Source Files/4task.cpp
@@ -161,7 +161,7 @@ int generalFunctionOfTaskFourLabFour() {
     
     int* array = new int[size];
     for (int i = 0; i < size; i++) {
-        array[i] = eng() % 20;
+        array[i] = eng() % 21;
     }
     
     bubleSort(array, size);
